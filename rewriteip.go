324f1399bf265bdba307304385c41a87750dec21
hrj-best <huangrenjie21@mails.ucas.ac.cn>
2025-02-20 15:43:48 +0800

    Update rewriteip.go

--- rewriteip.go
+++ rewriteip.go
@@ -5,6 +5,7 @@ import (
 	"net"
 
 	"github.com/coredns/coredns/plugin"
+	"github.com/coredns/coredns/plugin/pkg/dnstest"
 	"github.com/coredns/coredns/request"
 	"github.com/miekg/dns"
 )
@@ -18,24 +19,28 @@ type RewriteIP struct {
 func (ri RewriteIP) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
 	state := request.Request{W: w, Req: r}
 
-	// 调用下游插件（Unbound）
-	rcode, err := ri.Next.ServeDNS(ctx, w, r)
+	// 记录 Unbound 返回的响应
+	rec := dnstest.NewRecorder(w)
+	rcode, err := ri.Next.ServeDNS(ctx, rec, r)
 	if err != nil {
 		return rcode, err
 	}
 
-	// 获取客户端 IP
+	// 解析客户端 IP
 	clientIP := net.ParseIP(state.IP())
+	if clientIP == nil {
+		return rcode, nil // 如果获取失败，直接返回
+	}
 
-	// 遍历 DNS 响应，修改 A 记录
-	for _, ans := range r.Answer {
+	// 修改 A 记录
+	for _, ans := range rec.Msg.Answer {
 		if aRecord, ok := ans.(*dns.A); ok {
-			aRecord.A = clientIP.To4() // 替换为客户端 IP
+			aRecord.A = clientIP.To4() // 替换 A 记录
 		}
 	}
 
 	// 发送修改后的响应
-	w.WriteMsg(r)
+	w.WriteMsg(rec.Msg)
 	return rcode, nil
 }
 
